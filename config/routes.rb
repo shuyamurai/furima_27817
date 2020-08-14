650a13bdc6ee97bc01d10e3e06c1f1ac66dc6a64
shuyamurai <shuyamurai@yahoo.co.jp>
2020-08-14 20:46:46 +0900

    create items edit2

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,3 @@ Rails.application.routes.draw do
   
   resources :items, only: [:new, :create, :index, :show, :edit, :update]
 end
-
-
-
-
-
-
-# Rails.application.routes.draw do
-#   devise_for :users, controllers: {
-#     registrations: 'users/registrations',
-#     sessions: 'users/sessions'
-#   }
-#   root 'items#index'
-  
-#   resources :items, only: [:new, :create]
-# end

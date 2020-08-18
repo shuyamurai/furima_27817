1251ed48cb7b076a15545ae46161e5a576088adc
shuyamurai <shuyamurai@yahoo.co.jp>
2020-08-18 14:16:25 +0900

    create fix

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_41b4eefe069d51f89f27cdd2"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
